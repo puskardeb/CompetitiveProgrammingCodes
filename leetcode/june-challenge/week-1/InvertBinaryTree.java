34d0a99190106c22035e1e18865bd86b72619d90
Puskar Deb <puskardeb@gmail.com>
2020-06-01 17:58:17 +0530

    june challenge week 1 day 1

---
+++ leetcode/june-challenge/week-1/InvertBinaryTree.java
@@ -0,0 +1,12 @@
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null)   return root;
+        TreeNode right = invertTree(root.left);
+        TreeNode left = invertTree(root.right);
+
+        root.left = left;
+        root.right = right;
+
+        return root;
+    }
+}
