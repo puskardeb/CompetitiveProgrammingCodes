76e0af4a029e49f6fa332cb8afeaffe413490d2e
Puskar Deb <puskardeb@gmail.com>
2020-06-26 19:17:21 +0530

    week 4 day 5

---
+++ leetcode/june-challenge/week-3/sum_root_to_leaf_num.cxx
@@ -0,0 +1,23 @@
+class Solution {
+private:
+    vector<string> v;
+public:
+    int sumNumbers(TreeNode* root) {
+        int sum = 0;
+        return recur(root, sum);
+    }
+
+    int recur(TreeNode *root, int s) {
+        if (root == nullptr) {
+            return 0;
+        }
+        if (root->left == nullptr &&
+                root->right == nullptr) {
+            return s * 10 + root->val;
+        }
+        int left = recur(root->left, s * 10 + root->val);
+        int right = recur(root->right, s * 10 + root->val);
+
+        return left + right;
+    }
+};
