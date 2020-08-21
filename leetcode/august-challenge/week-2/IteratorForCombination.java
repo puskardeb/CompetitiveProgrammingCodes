2033682c4a48fe62ab6e6b6d9a2af5bc98536afd
Puskar Deb <puskardeb@gmail.com>
2020-08-21 17:47:39 +0530

    week 2 day 6

---
+++ leetcode/august-challenge/week-2/IteratorForCombination.java
@@ -0,0 +1,41 @@
+public class Subsets {
+    char[] str;
+    List<String> outer;
+    int idx;
+
+    public Subsets(String characters, int combinationLength) {
+        str = characters.toCharArray();
+        outer = new ArrayList<>();
+        idx = 0;
+        int n = str.length;
+        for (int mask = 0; mask < (1 << n); ++mask) {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < n; ++i) {
+                if ((mask & (1 << i)) != 0) {
+                    sb.append(str[i]);
+                }
+            }
+            if (sb.length() == combinationLength)
+                outer.add(sb.toString());
+        }
+        Collections.sort(outer);
+        System.out.println(outer);
+    }
+
+    public String next() {
+        StringBuilder sb = new StringBuilder();
+        while (idx < outer.size()) {
+            sb.append(outer.get(idx));
+            ++idx;
+            return sb.toString();
+        }
+        return sb.toString();
+    }
+
+    public boolean hasNext() {
+        if (idx == outer.size())
+            return false;
+        return true;
+    }
+}
+
